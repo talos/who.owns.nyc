e158efac098c03e67deb2bbeb136b19689dc1162
John Krauss <irving.krauss@gmail.com>
2014-11-05 17:31:47 -0500

    functional

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-$.ajax({
-    url: "http://data.acgov.org/resource/k9se-aps6.json?city=Alameda",
+var rows = [];
+
+var search = function (evt) {
+  evt.stopPropagation();
+  var borough = $('#borough').val();
+  var block = $('#block').val();
+  var lot = $('#lot').val();
+
+  // Obtain property JSON
+  $.ajax({
+    url: "http://data.cityofnewyork.us/resource/8h5j-fqxa.json?$where=borough=" +
+      borough + "%20and%20block=" + block + "%20and%20lot=" + lot,
     jsonp: "$jsonp",
     dataType: "jsonp"
-}).done(function(data) {
-    console.log("Request received: " + data);
+  }).done(function(data) {
+    //Array.prototype.push.apply(rows, data);
+    $('#output').text(JSON.stringify(rows));
+
+    // TODO: obtain master and party data from this
+  });
+  return false;
+};
+
+$(document).ready(function () {
+  $('#form').on('submit', search);
 });
